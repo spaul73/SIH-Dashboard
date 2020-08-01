9a775a105ef9eba95082db6c014dd410b77fac70
Shashwat <shashwatpaul@gmail.com>
2020-08-02 00:08:21 +0530

    Added District in SchoolsList

--- SIH Dashboard/wwwroot/js/displaytable.js
+++ SIH Dashboard/wwwroot/js/displaytable.js
@@ -2,4 +2,8 @@
     $(table).DataTable({
         "searching": searching
     });
+    $.fn.dataTable.ext.errMode = 'none';
+    $(table).on('error.dt', function (e, settings, techNote, message) {
+        console.log('An error has been reported by DataTables: ', message);
+    });
 }
