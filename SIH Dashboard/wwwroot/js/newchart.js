575b3a911b187e039bb0e798975b8cf93322d2d1
Shashwat <shashwatpaul@gmail.com>
2020-08-03 13:57:11 +0530

    Updated SchoolOverview

--- SIH Dashboard/wwwroot/js/newchart.js
+++ SIH Dashboard/wwwroot/js/newchart.js
@@ -46,6 +46,56 @@
         }
     });
 }
+function DisplayColumnChart2(id, lab, labs, dat) {
+    document.getElementById("allcontainer").innerHTML = '&nbsp;';
+    document.getElementById("allcontainer").innerHTML = '<canvas id="' + id + '" style="max-width: 800px;"></canvas>';
+    var ctx = document.getElementById(id).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labs,
+            datasets: [{
+                label: lab,
+                data: dat,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    },
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false
+                    }
+                }]
+
+            }
+        }
+    });
+}
 function DisplayLineChart(id, dates, cat1, cat2, cat3, cat4, cat5, cat6) {
     //document.getElementById("graph-container").innerHTML = '&nbsp;';
     //document.getElementById("graph-container").innerHTML = '<canvas id="' + id + '" style="max-width: 200px;"></canvas>';
